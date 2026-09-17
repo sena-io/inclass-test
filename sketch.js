f622ba54557de4c0176134176f7cb17323471d8b
Sen <senahchoi@gmail.com>
2026-09-16 18:41:56 -0700

    Changed the sketch

---
+++ sketch.js
@@ -0,0 +1,11 @@
+console.log("I believe I can do this!");
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(20);
+  fill(255, 200, 60);
+  circle(mouseX, mouseY, 100);
+}
